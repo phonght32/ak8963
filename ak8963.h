31d0c580da13065563242817b7032c45dcdefe31
phonght32 <thanhphongho1998@gmail.com>
2024-02-24 23:31:21 +0700

    add define i2c address

--- ak8963.h
+++ ak8963.h
@@ -29,6 +29,8 @@ extern "C" {
 
 #include "err_code.h"
 
+#define AK8963_I2C_ADDR  		0x0C
+
 typedef err_code_t (*ak8963_func_i2c_send)(uint8_t reg_addr, uint8_t *buf_send, uint16_t len);
 typedef err_code_t (*ak8963_func_i2c_recv)(uint8_t reg_addr, uint8_t *buf_recv, uint16_t len);
 typedef void (*ak8963_func_delay)(uint32_t ms);
